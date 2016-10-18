f4be5123d9a9cb114a64cec4dc71e7eb95b2fdd5
Charles Araya <charlesaraya@gmail.com>
2016-10-18 14:56:42 +0200

    feat(university.js): add serious functionality for show off

--- examples/university.js
+++ examples/university.js
@@ -44,8 +44,50 @@ const actions = {
     return new Promise(function (resolve, reject) {
       var typeInfo = firstEntityValue(entities, 'typeInfo')
       var staff = firstEntityValue(entities, 'staff')
-      // university things here
-      context.result = typeInfo + staff
+      if (staff && typeInfo) {
+        switch (typeInfo) {
+          case 'despacho':
+            context.result = 'DKV-5' // we should call a Staff API here
+            delete context.unknownTypeinfo
+            break;
+          case 'email':
+            context.result = 'charlesaraya@gmail.com' // we should call a Staff API here
+            delete context.unknownTypeinfo
+            break;
+          case 'asignaturas':
+            context.result = 'Electrónica, Patrones de Comunicación y Redes III' // we should call a Staff API here
+            delete context.unknownTypeinfo
+            break;
+          case 'telefono':
+            context.result = '628111020' // we should call a Staff API here
+            delete context.unknownTypeinfo
+            break;
+          case 'fax':
+            context.result = '914552113' // we should call a Staff API here
+            delete context.unknownTypeinfo
+            break;
+          case 'web':
+            context.result = 'http://charlesaraya.com' // we should call a Staff API here
+            delete context.unknownTypeinfo
+            break;
+          default:
+            delete context.result
+            context.unknownTypeinfo = typeInfo
+        }
+        delete context.error
+        delete context.missingProfessor
+        // we should call search the person in the staff Api
+        if (staff !== 'Charles Araya') {
+          delete context.result
+          delete context.unknownTypeinfo
+          context.missingProfessor = staff
+        }
+      } else {
+        delete context.result
+        delete context.unknownTypeinfo
+        delete context.missingProfessor
+        context.error = true
+      }
       return resolve(context)
     })
   }
