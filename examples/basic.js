a52e16298a183b5b19b3a92486748d1b74863ee9
Charles Araya <charlesaraya@gmail.com>
2016-10-24 09:08:05 +0200

    style(lib/basic.js): update to meet javascript standard style convention

--- examples/basic.js
+++ examples/basic.js
@@ -1,35 +1,37 @@
-'use strict';
+'use strict'
 
-let Wit = null;
-let interactive = null;
+let Wit = null
+let interactive = null
+
 try {
   // if running from repo
-  Wit = require('../').Wit;
-  interactive = require('../').interactive;
+  Wit = require('../').Wit
+  interactive = require('../').interactive
 } catch (e) {
-  Wit = require('node-wit').Wit;
-  interactive = require('node-wit').interactive;
+  Wit = require('node-wit').Wit
+  interactive = require('node-wit').interactive
 }
 
 const accessToken = (() => {
   if (process.argv.length !== 3) {
-    console.log('usage: node examples/basic.js <wit-access-token>');
-    process.exit(1);
+    console.log('usage: node examples/basic.js <wit-access-token>')
+    process.exit(1)
   }
-  return process.argv[2];
-})();
+  return process.argv[2]
+})()
 
 const actions = {
-  send(request, response) {
-    const {sessionId, context, entities} = request;
-    const {text, quickreplies} = response;
-    return new Promise(function(resolve, reject) {
-      console.log('user said...', request.text);
-      console.log('sending...', JSON.stringify(response));
-      return resolve();
-    });
-  },
-};
+  send (request, response) {
+    const {sessionId, context, entities} = request
+    const {text, quickreplies} = response
 
-const client = new Wit({accessToken, actions});
-interactive(client);
+    return new Promise(function (resolve, reject) {
+      console.log('user said...', request.text)
+      console.log('sending...', JSON.stringify(response))
+      return resolve()
+    })
+  }
+}
+
+const client = new Wit({accessToken, actions})
+interactive(client)
